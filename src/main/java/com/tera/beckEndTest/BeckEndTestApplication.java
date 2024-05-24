6a83c0cfbb3965a7e7c5d7b049573678de07f387
incheons <sonincheon94@naver.com>
2024-05-24 13:28:17 +0900

    2024/05/24 srping boot, h2데이터베이스 , 마리아DB 기본세팅

---
+++ src/main/java/com/tera/beckEndTest/BeckEndTestApplication.java
@@ -0,0 +1,12 @@
+package com.tera.beckEndTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeckEndTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BeckEndTestApplication.class, args);
+	}
+
+}
